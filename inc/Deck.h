7302020d6bd7a5edc2cf8dc1cf8f7ed79d95596a
張永怡 <yongyi.ee13@nycu.edu.tw>
2025-05-11 12:00:35 +0800

    Revert "Revert "create header""

---
+++ inc/Deck.h
@@ -0,0 +1,17 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <vector>
+#include "Card.h"
+
+class Deck {
+private:
+    std::vector<Card> cards;
+public:
+    Deck();
+    void shuffle();
+    std::vector<Card> deal(int n);
+    void reset();
+};
+
+#endif
---
+++ inc/Scorer.h
@@ -0,0 +1,13 @@
+#ifndef SCORER_H
+#define SCORER_H
+
+#include <vector>
+#include <string>
+#include "Card.h"
+
+class Scorer {
+public:
+    static std::pair<std::string, int> evaluate(const std::vector<Card>& playedCards);
+};
+
+#endif
